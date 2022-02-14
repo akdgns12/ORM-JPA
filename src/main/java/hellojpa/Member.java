7f1276d42a8d5e9cc4c45ecf6ff89dff9bf84463
akdgns12 <akdgns12@naver.com>
2022-02-14 12:10:33 +0900

    회원 삭제

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 @Entity // JPA가 관리할 객체
 public class Member {
 
-    @Id // JPA에게 pk가 뭔지 알려주는 역할(데이터베이스 PK와 매핑)
+    @Id // JPA에게 pk가 뭔지 알려주는 역할(데이터베이스 PK와 매핑)t
     private Long id;
     private String name;
 
