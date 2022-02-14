349b820bc5bbf0a3d4a18a753e94dcbfa59f3da6
akdgns12 <akdgns12@naver.com>
2022-02-14 12:06:28 +0900

    회원 등록

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // 앤티티 매니저 팩토리는 하나만 생성해서 프로젝트 전체에서 공유
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 앤티티 매니저는 쓰레드간에 공유 X (사용하고 버려야 한다)
+        EntityManager em = emf.createEntityManager();
+        // JPA의 모든 데이터 변경은 Transaction안에서 실행!
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            // 앤티티 매니저는 쓰레드간에 공유 X (사용하고 버려야 한다)
+            em.close();
+        }
+
+        emf.close();
+    }
+}
