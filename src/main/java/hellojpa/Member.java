7f55f234899af8f493c64299ff4e2937ef126714
akdgns12 <akdgns12@naver.com>
2022-02-15 14:22:39 +0900

    객체와 테이블 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,14 +2,16 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity // JPA가 관리할 객체
+@Entity // JPA가 관리할 객체, JPA를 사용해서 테이블과 매핑할 클래스는 @Entity 필수
+//@Table(name = "MBR") // 엔티티와 매핑할 테이블 지정
 public class Member {
 
-    @Id // JPA에게 pk가 뭔지 알려주는 역할(데이터베이스 PK와 매핑)t
+    @Id // JPA에게 pk가 뭔지 알려주는 역할(데이터베이스 PK와 매핑)
     private Long id;
     private String name;
-
+    // *주의 - 기본 생성자 필수(파라미터가 없는 public 또는 protected 생성자)
     public Member(){
     }
 
