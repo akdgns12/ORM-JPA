0f6d6505e7ed1adf825da8a36da295f8a7d32adf
akdgns12 <akdgns12@naver.com>
2022-02-22 13:35:35 +0900

    기본값 타입

---
+++ src/main/java/hellojpa/valueMain.java
@@ -0,0 +1,21 @@
+package hellojpa;
+
+public class valueMain {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = a;
+
+        b = 20;
+
+        Integer c = new Integer(10);
+        Integer d = c;
+
+//        c.setValue()
+        // 이런식으로 자바 기본값 타입은 절대 공유될 수 없다(서로 다른 저장소를 가지고 있다), 기본 타입은 항상 값을 복사함
+        // But, Integer같은 래퍼클래스나 String같은 특수한 클래스는 공유가능한 객체이지만 변경은 X(값을 복사하는게 아닌 주소를
+        // 참조하기 때문에!)
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
